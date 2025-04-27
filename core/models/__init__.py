154aee71ba16f44fadad35be51bc0425b70cb7d7
Leshik1305 <realleshik1305@gmail.com>
2025-04-27 18:10:14 +0300

    migration for users table

--- core/models/__init__.py
+++ core/models/__init__.py
@@ -4,9 +4,11 @@ __all__ = {
     "DatabaseHelper",
     "Product",
     "User",
+    "Post",
 }
 
 from .base import Base
 from .db_helper import db_helper, DatabaseHelper
 from .product import Product
 from .user import User
+from .post import Post
