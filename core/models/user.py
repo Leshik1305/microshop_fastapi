ea200a5aed1ad4a4f731c591baa8562e3d34e421
Leshik1305 <realleshik1305@gmail.com>
2025-04-28 13:19:28 +0300

    create user  relation mixin and profile

--- core/models/user.py
+++ core/models/user.py
@@ -1,4 +1,5 @@
 from typing import TYPE_CHECKING
+
 from sqlalchemy import String
 from sqlalchemy.orm import Mapped, mapped_column, relationship
 
@@ -6,7 +7,11 @@ from .base import Base
 
 if TYPE_CHECKING:
     from .post import Post
+    from .profile import Profile
+
+
 class User(Base):
     username: Mapped[str] = mapped_column(String(32), unique=True)
-    posts : Mapped[ list["Post" ]] = relationship(back_populates="user")
 
+    posts: Mapped[list["Post"]] = relationship(back_populates="user")
+    profile: Mapped["Profile"] = relationship(back_populates="user")
