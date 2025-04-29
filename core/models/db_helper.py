5679b82c0476a3f24a2123bcdb5dc4a8823e3289
Leshik1305 <realleshik1305@gmail.com>
2025-04-29 13:11:28 +0300

    add crud

--- core/models/db_helper.py
+++ core/models/db_helper.py
@@ -1,11 +1,11 @@
+from asyncio import current_task
+
 from sqlalchemy.ext.asyncio import (
+    AsyncSession,
     create_async_engine,
     async_sessionmaker,
     async_scoped_session,
-    AsyncSession,
 )
-from asyncio import current_task
-
 
 from core.config import settings
 
@@ -23,7 +23,7 @@ class DatabaseHelper:
             expire_on_commit=False,
         )
 
-    def get_scope_session(self):
+    def get_scoped_session(self):
         session = async_scoped_session(
             session_factory=self.session_factory,
             scopefunc=current_task,
@@ -36,12 +36,12 @@ class DatabaseHelper:
             await session.close()
 
     async def scoped_session_dependency(self) -> AsyncSession:
-        session = self.get_scope_session()
+        session = self.get_scoped_session()
         yield session
         await session.close()
 
 
 db_helper = DatabaseHelper(
-    url=settings.db_url,
-    echo=settings.db_echo,
+    url=settings.db.url,
+    echo=settings.db.echo,
 )
