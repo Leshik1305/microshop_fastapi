a8742f24c8af0d67f9c42a8ef218b233ff9f2a9b
Leshik1305 <realleshik1305@gmail.com>
2025-05-04 12:30:43 +0300

    add demo_auth_token

--- api_v1/demo_auth/views.py
+++ api_v1/demo_auth/views.py
@@ -1,7 +1,7 @@
 import secrets
 from typing import Annotated
 
-from fastapi import APIRouter, Depends, HTTPException, status
+from fastapi import APIRouter, Depends, HTTPException, status, Header
 from fastapi.security import HTTPBasic, HTTPBasicCredentials
 
 router = APIRouter(prefix="/demo-auth", tags=["Demo Auth"])
@@ -9,7 +9,7 @@ router = APIRouter(prefix="/demo-auth", tags=["Demo Auth"])
 security = HTTPBasic()
 
 
-@router.get("/basic-auth")
+@router.get("/basic-auth/")
 def demo_basic_auth_credentials(
     credentials: Annotated[HTTPBasicCredentials, Depends(security)],
 ):
@@ -20,7 +20,15 @@ def demo_basic_auth_credentials(
     }
 
 
-username_to_passwords = {"admin": "admin", "john": "password"}
+username_to_passwords = {
+    "admin": "admin",
+    "john": "password",
+}
+
+static_auth_token_to_username = {
+    "fasfadfgsdf": "admin",
+    "fdsfsdafsdfasdf": "john",
+}
 
 
 def get_auth_user_username(
@@ -42,7 +50,18 @@ def get_auth_user_username(
     return credentials.username
 
 
-@router.get("/basic-auth-username")
+def get_username_by_static_auth_token(
+    static_token: str = Header(alias="x-auth-token"),
+) -> str:
+    if static_token not in static_auth_token_to_username:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="token invalid",
+        )
+    return static_auth_token_to_username[static_token]
+
+
+@router.get("/basic-auth-username/")
 def demo_basic_auth_username(
     auth_username: str = Depends(get_auth_user_username),
 ):
@@ -50,4 +69,15 @@ def demo_basic_auth_username(
     return {
         "message": f"Hi, {auth_username}!",
         "username": auth_username,
+    }
+
+
+@router.get("/some-http-header-auth/")
+def demo_auth_some_http_header(
+    username: str = Depends(get_username_by_static_auth_token),
+):
+
+    return {
+        "message": f"Hi, {username}!",
+        "username": username,
     }
